bbcb294b990a733024067faadcf70b7c42a2fcf8
tkulic <toni.kulic@yahoo.com>
2021-06-03 16:06:29 +0200

    Markup and styles finished

---
+++ index.js
@@ -0,0 +1,9 @@
+const searchButton = document.getElementById("search-button")
+const ipAddress = document.getElementById("ip-address")
+const location = document.getElementById("location")
+const timezone = document.getElementById("timezone")
+const isp = document.getElementById("isp")
+
+searchButton.addEventListener("click", () => {
+    console.log("clicked")
+})
